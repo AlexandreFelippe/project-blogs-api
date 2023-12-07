7e6b1ed409592372eabec6d17c7528eb65616602
AlexandreFelippe <alemfelippe@gmail.com>
2023-12-07 15:15:42 -0300

    Requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,23 @@
+const Categories = (sequelize, DataTypes) => {
+const Category = sequelize.define('Category', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  timestamps: false,
+  tableName: 'categories',
+});
+
+return Category;
+
+};
+
+module.exports = Categories;
